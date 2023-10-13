cd3ba0214ca44313ba32ac422b8a3e161e105df7
Sebastian Bula <128990941+sbulac@users.noreply.github.com>
2023-10-13 11:47:34 +0000

    feat: nuevo diseño de peliculas

--- src/components/SingleMovie/index.jsx
+++ src/components/SingleMovie/index.jsx
@@ -12,26 +12,46 @@ const SingleMovie = ({ poster, title }) => {
     <Box
       sx={{ display: "flex", alignItems: "center", justifyContent: "center" }}
     >
-      <Card sx={{ width: "max-content", borderRadius: "15px", maxWidth: 231 }}>
+      <Card
+        sx={{
+          width: "max-content",
+          maxWidth: 231,
+          backgroundColor: "transparent",
+          boxShadow: "none",
+        }}
+      >
         <CardActionArea
           sx={{
             display: "flex",
             alignItems: "center",
             justifyContent: "space-between",
             flexDirection: "column",
+            position: "relative",
           }}
         >
           <CardMedia
             component="img"
             image={`https://image.tmdb.org/t/p/w500/${poster}`}
             sx={{
+              borderRadius: "15px",
               height: 340.5,
               objectFit: "cover",
               flexGrow: 1,
             }}
           />
-          <Box sx={{ p: 1, textAlign: "center"}}>
-            <Typography variant="h6">{title}</Typography>
+          <Box
+            sx={{
+              height: 340.5,
+              width: "100%",
+              position: "absolute",
+              background: "linear-gradient(180deg, rgba(0,0,0,1) 0%, rgba(0,212,255,0) 70%)"
+            }}
+          >
+            <Box sx={{ p: 1, textAlign: "start", color: "#EEE" }}>
+              <Typography sx={{ fontSize: 18 }} variant="h6">
+                {title}
+              </Typography>
+            </Box>
           </Box>
         </CardActionArea>
       </Card>
