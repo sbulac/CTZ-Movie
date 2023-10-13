0565b7a709949ef819d6975a249af91f09aca053
sbulac <sb.bulac@gmail.com>
2023-10-13 15:20:34 -0500

    feat: movie details info

--- src/components/DetailsMovie/index.jsx
+++ src/components/DetailsMovie/index.jsx
@@ -2,11 +2,12 @@ import React, { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { ApiContext } from "../Context/Context";
 import axios from "axios";
+import { Box, Typography } from "@mui/material";
 
 const DetailsMovie = () => {
   const { id } = useParams();
   const [movie, setMovie] = useState({});
-  const { setLoading } = useContext(ApiContext);
+  const { setLoading, loading } = useContext(ApiContext);
   let url = `https://api.themoviedb.org/3/movie/${id}?language=en-US`;
 
   const options = {
@@ -32,7 +33,43 @@ const DetailsMovie = () => {
     }
   }, [id]);
   console.log(movie);
-  return <div>DetailsMovie {id}</div>;
+  return (
+    <Box>
+      {loading ? (
+        <span></span>
+      ) : (
+        <>
+          <Box
+            component="img"
+            width={250}
+            height={400}
+            src={`https://image.tmdb.org/t/p/w500/${movie["poster_path"]}`}
+          />
+          <Typography>{movie["original_title"]}</Typography>
+          <br />
+          <Typography>{movie.overview}</Typography>
+          <br />
+          {movie["production_companies"].map((item, index) => (
+            <Box key={index}>
+              <Typography>{item.name}</Typography>
+              {item["logo_path"] ? (
+                <Box
+                  component="img"
+                  src={`https://image.tmdb.org/t/p/w500/${item["logo_path"]}`}
+                />
+              ) : (
+                <span>No Image</span>
+              )}
+            </Box>
+          ))}
+          <br />
+          {movie["production_countries"].map((item) => (
+            <Typography key={item.name}>{item.name}</Typography>
+          ))}
+        </>
+      )}
+    </Box>
+  );
 };
 
 export default DetailsMovie;
